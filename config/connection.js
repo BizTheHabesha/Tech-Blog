ff247a15fe12ea4e48ff0fecc6c90f507bc38d82
Biz <github@bisrat.dev>
2023-05-25 11:33:35 -0600

    config first

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,9 @@
-// import sequelize
-const Sequelize = require("sequelize");
 // config .env
 require("dotenv").config();
+// import sequelize
+const Sequelize = require("sequelize");
 // initialize sequlize var to export later
 let sequelize;
-
 if (process.env.JAWSDB_URL) {
 	// if jawsdb url is defined, use jawsdb to initialize Sequelize
 	sequelize = new Sequelize(process.env.JAWSDB_URL);
@@ -13,7 +12,7 @@ if (process.env.JAWSDB_URL) {
 	sequelize = new Sequelize(
 		process.env.DB_NAME,
 		process.env.DB_USER,
-		process.env.DB_PW,
+		process.env.DB_PASSWORD,
 		{
 			host: "localhost",
 			dialect: "mysql",
