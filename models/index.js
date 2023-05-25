cf86bed112ea0a33c879174946a5cf59a27b072d
Biz <github@bisrat.dev>
2023-05-25 08:29:05 -0600

    create relationships

--- models/index.js
+++ models/index.js
@@ -1,3 +1,12 @@
 const User = require("./User");
+const Post = require("./Post");
+const Comment = require("./Comment");
 
-module.exports = { User };
+User.hasMany(Post);
+User.hasMany(Comment);
+Post.belongsTo(User);
+Post.hasMany(Comment);
+Comment.belongsTo(User);
+Comment.belongsTo(Post);
+
+module.exports = { User, Post, Comment };
