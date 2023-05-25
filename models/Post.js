5661d31e7aa8ad2cc6ad3d52ee32f3e278d1b7e3
Biz <github@bisrat.dev>
2023-05-25 07:10:20 -0600

    add post model

---
+++ models/Post.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config");
+
+class Post extends Model {}
+
+Post.init(
+	{
+		// unique id for every post autoincremented to prevent reusing an ID
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		// post title that will appear on the webpage
+		title: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		// post body that will appear on the webpage
+		body: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		// reference to the user who posted this blog post
+		author_id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			references: {
+				model: "user",
+				key: "id",
+			},
+		},
+	},
+	{
+		// connect to sequelize
+		sequelize,
+		// use timestamps
+		timestamps: true,
+		// table name matches model name
+		freezeTableName: true,
+		// underscores for spaces
+		underscored: true,
+		// model name for references
+		modelName: "post",
+	}
+);
+
+module.exports = Post;
