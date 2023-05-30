607a1eef7ba689d372b1e5f42a308ec24c56210a
Biz <github@bisrat.dev>
2023-05-29 18:23:30 -0600

    hardcode seed data

--- seeds/postData.js
+++ seeds/postData.js
@@ -2,26 +2,31 @@ const { Post } = require("../models");
 
 const seedData = [
 	{
+		id: 1,
 		title: "First post!",
 		body: "The first post on the blog! How's everyone doing?",
 		author_id: 1,
 	},
 	{
+		id: 2,
 		title: "Second post!",
 		body: "How the hell did Biz beat me to the first? And what is that name??",
 		author_id: 2,
 	},
 	{
+		id: 3,
 		title: "What is this?",
 		body: "Hey everyone, just found this. What going on!",
 		author_id: 3,
 	},
 	{
+		id: 4,
 		title: "What are you guys up to?",
 		body: "Just curious what everyone's up to right now?",
 		author_id: 1,
 	},
 	{
+		id: 5,
 		title: "Lot of gamers!",
 		body: "Looks like you're all gamers! How about we dm on dis and get some games going?",
 		author_id: 1,
--- seeds/userData.js
+++ seeds/userData.js
@@ -2,16 +2,19 @@ const { User } = require("../models");
 
 const seedData = [
 	{
+		id: 1,
 		name: "Biz Gebremeskelhaymnotyemichaellidetamlakinayeexhabrinsim",
 		email: "email@gmail.com",
 		password: "password123",
 	},
 	{
+		id: 2,
 		name: "Jossi Hossi",
 		email: "joss@gmail.com",
 		password: "ihavebadpasswords",
 	},
 	{
+		id: 3,
 		name: "Michael Stranbrought",
 		email: "mstranbrought@gmail.com",
 		password: "greatpassword!",
