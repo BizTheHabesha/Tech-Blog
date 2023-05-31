2f92da9a5964b3979114b220b5dbfe3f30326996
Biz <github@bisrat.dev>
2023-05-31 05:15:37 -0600

    add post deletion handling

--- public/js/deletepost.js
+++ public/js/deletepost.js
@@ -1,18 +1,16 @@
-const deletepost = async (e) => {
+console.log("deletepost.js : initialized");
+$(".delete-post-btn").click(async function (e) {
+	console.log("deletepost.js : clicked");
 	e.preventDefault();
-
-	const res = await fetch("/api/posts/", {
+	const res = await fetch(`/api/posts/${$(e.target).data("id")}`, {
 		method: "DELETE",
 		headers: { "Content-Type": "application/json" },
 	});
 
+	console.log(`deletepost.js : res => ${JSON.stringify(res)}`);
 	if (res.ok) {
-		document.location.reload();
+		document.location.replace("/dashboard");
 	} else {
 		alert(`There was an error: ${res.statusText}`);
 	}
-};
-
-document
-	.querySelector(".delete-post-btn")
-	.addEventListener("click", deletepost);
+});
