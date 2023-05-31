b0254dbdef8b4660cc4edf0c75f117e43a1a6069
Biz <github@bisrat.dev>
2023-05-31 05:16:35 -0600

    refactor for jquery

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,12 +1,13 @@
-const signupFormHandler = async (event) => {
-	event.preventDefault();
+$(".signup-btn").click(async function (e) {
+	e.preventDefault();
+	const name = $("#name-form-input").val().trim();
+	const email = $("#email-form-input").val().trim();
+	const password = $("#passowrd-form-input").val().trim();
 
-	const name = document.querySelector("#name-signup").value.trim();
-	const email = document.querySelector("#email-signup").value.trim();
-	const password = document.querySelector("#password-signup").value.trim();
+	console.log(`NAME: ${name} | EMAIL: ${email} | PASSWORD: ${password}`);
 
 	if (name && email && password) {
-		const response = await fetch("/api/users", {
+		const response = await fetch("/api/users/", {
 			method: "POST",
 			body: JSON.stringify({ name, email, password }),
 			headers: { "Content-Type": "application/json" },
@@ -15,11 +16,10 @@ const signupFormHandler = async (event) => {
 		if (response.ok) {
 			document.location.replace("/dashboard");
 		} else {
+			console.log(JSON.stringify(response));
 			alert(response.statusText);
 		}
+	} else {
+		alert("Please enter a valid email, name, and password!");
 	}
-};
-
-document
-	.querySelector(".signup-form")
-	.addEventListener("submit", signupFormHandler);
+});
